0a2e33dd037d40f4e495f185f62c49d09fb3da6b
Sean <roberts.sean@gmail.com>
2012-02-25 18:52:30 -0500

    Use keys for auth

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -1,4 +1,4 @@
 require 'omniauth/strategies/steam'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, '3606FB72D46F2509FA118C98B902D541'
+  provider :steam, ENV['STEAM_WEB_API_KEY']
 end
